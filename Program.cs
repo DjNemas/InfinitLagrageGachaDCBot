e8c0c9625f6bae0cde8bfb493c5c16ec4ccc6501
Denis Kliem <denis_kliem@yahoo.de>
2021-10-18 04:24:10 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InfinitLagrageGachaDCBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
